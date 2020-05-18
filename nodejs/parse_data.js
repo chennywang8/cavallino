bdbdbaf4b5884fc3f21acae42d73e79d6ee10259
Chenny Wang <lotharwang@gmail.com>
2020-05-17 23:19:03 -0700

    accumulate buffer for discontinuous handling

--- nodejs/parse_data.js
+++ nodejs/parse_data.js
@@ -2,21 +2,15 @@
 const sample_size   = 4;
 const buffer_size   = 8 * sample_size;
 let data            = [0, 0, 0, 0];
-let binBuffer       = '';
+let buffer          = Buffer.alloc(0, 0, 'binary');
 
 function parse_data(raw) {
-    // FIX ME, need to accumulate
-    binBuffer = raw;
-    if (binBuffer.length >= buffer_size) {
-        let buffer = Buffer.from(binBuffer, 0, buffer_size);
-        if (binBuffer.length > buffer_size) {
-            binBuffer = binBuffer.substring(buffer_size);
-        } else {
-            binBuffer = '';
-        }
+    buffer = Buffer.concat([buffer, raw]);
+    if (buffer.length >= buffer_size) {
         for (let i = 0; i < sample_size; i++) {
             data[i] = buffer.readDoubleLE(i*8);
         }
+        buffer = buffer.subarray(buffer_size);
     } 
     return data;
 }
